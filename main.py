a2f1dd78c42ae5b4d3fda46265dacb4c3e391c72
rohan darji <rohan.darji12397@gmail.com>
2023-04-12 12:39:09 +0530

    read data from database

---
+++ main.py
@@ -0,0 +1,10 @@
+from sensor.logger import logging
+from sensor.exception import SensorException
+import sys
+from sensor.utils import get_collection_as_dataframe
+    
+if __name__ == '__main__':
+    try:
+        get_collection_as_dataframe(database_name="aps",collection_name="sensor")
+    except Exception as e:
+        print(e)
---
+++ sensor/config.py
@@ -0,0 +1,13 @@
+import pymongo
+import pandas as pd
+import json
+from dataclasses import dataclass
+import os
+
+@dataclass
+class EnvironmentVariable():
+    mongo_db_url:str = os.getenv("MONGO_DB_URL")
+
+env_Var = EnvironmentVariable()
+
+mongo_client = pymongo.MongoClient(env_Var.mongo_db_url)
